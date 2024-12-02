ef9c576dcacebe7afed022329de673345bda20f6
RahulKaushal09 <rahul82rahul2002@gmail.com>
2024-12-02 14:10:48 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 function App() {
+  const handleLoginSuccess = (response) => {
+    console.log('Login Success:', response);
+    // You can send the response to your backend for token verification and user info retrieval.
+  };
+
+  const handleLoginFailure = (error) => {
+    console.error('Login Failed:', error);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider >
+      <div className="App">
+        <h1>Login with Google</h1>
+        <GoogleLogin
+          onSuccess={handleLoginSuccess}
+          onError={handleLoginFailure}
+          useOneTap
+        />
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
